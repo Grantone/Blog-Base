bf25e3643e1a5d92017ab0a9d5cb9c6a5cae5862
Granton Mchana <gmchana254@gmail.com>
2017-11-03 15:09:46 +0300

    created a form for signing in and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
-from flask import Blueprint
-main = Blueprint('main', __name__)
-from . import views, error
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+# Initializing application
 app = Flask(__name__)
 
+# Setting up configuration
+app.config.from_object(config_options[config_name])
+
+bootstrap.init_app(app)
+db.init_app(app)
+login_manager.init_app(app)
 from app import views
