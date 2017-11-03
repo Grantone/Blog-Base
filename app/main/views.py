6d3d9ab6fca904254f3d3d8f362990466de75cd3
Granton Mchana <gmchana254@gmail.com>
2017-11-03 16:30:10 +0300

    created two template files comment.html and new_comment.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_tenplate
 from app import app
-
+from flask_login import login_required
 # Views
 
 
@@ -11,3 +11,20 @@ def index():
     '''
 
     return render_tenplate('index.html')
+
+
+@main.route('blog/comment/new<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+    blog = get_blog(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        comment = form.comment.data
+        new_comment = Comment(blog.id, title, comment)
+        new_comment.save_comment()
+        return redirect(url_for('blog', id=blog.id))
+
+    # title = f'{category.name} comment'
+    return render_template('new_comment.html', title=title, comment_form=form, blog=blog)
